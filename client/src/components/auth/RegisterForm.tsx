3c3dfabf5b54dddc842c2da7df3faa57926e9be6
Mohammed  CHAHBOUN <69054810+M97Chahboun@users.noreply.github.com>
2025-04-06 20:22:45 +0000

    fixed register page bug

--- client/src/components/auth/RegisterForm.tsx
+++ client/src/components/auth/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
