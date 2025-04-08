923cf221481a789ff63197478cd39747cad145de
Mohammed <m97.chahboun@gmail.com>
2025-04-08 12:22:36 +0100

    fixed minor bug

--- server/index.ts
+++ server/index.ts
@@ -81,21 +81,25 @@ app.use((req, res, next) => {
   const port = 5000;
 
   if (process.env.NODE_ENV !== 'production') {
-    app.listen(port, () => {
+    server.listen({
+      port,
+      host: "127.0.0.1",
+      reusePort: true,
+    }, () => {
       console.log('Server running on http://localhost:3000');
     });
+  } else {
+    // ALWAYS serve the app on port 5000
+    // this serves both the API and the client.
+    // It is the only port that is not firewalled.
+    server.listen({
+      port,
+      host: "0.0.0.0",
+      reusePort: true,
+    }, () => {
+      log(`serving on port ${port}`);
+    });
   }
-
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
 })();
 
 export default app;
